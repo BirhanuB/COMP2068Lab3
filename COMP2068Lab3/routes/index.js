66fe23301b847c0395467942f435ce957274b44c
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2020-06-11 18:23:14 -0400

    Revised format and added siblings info

--- COMP2068Lab3/routes/index.js
+++ COMP2068Lab3/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'My Family' });
+    res.render('index', { name: 'My Family' });
 });
 
 /* GET mom's page. */
@@ -15,6 +15,31 @@ router.get('/abebech', function (req, res) {
 /* GET dad's page. */
 router.get('/bogale', function (req, res) {
     res.render('bogale', { name: 'Bogale' });
+});
+
+/* GET older sister's page. */
+router.get('/abiyot', function (req, res) {
+    res.render('abiyot', { name: 'Abiyot' });
+});
+
+/* GET other older sister's page. */
+router.get('/adey', function (req, res) {
+    res.render('adey', { name: 'Adey' });
+});
+
+/* GET my page. */
+router.get('/birhanu', function (req, res) {
+    res.render('birhanu', { name: 'Birhanu' });
+});
+
+/* GET younger brother's page. */
+router.get('/wondwosen', function (req, res) {
+    res.render('wondwosen', { name: 'Wondwosen' });
+});
+
+/* GET younger sister's page. */
+router.get('/yeabsira', function (req, res) {
+    res.render('yeabsira', { name: 'Yeabsira' });
 });
 
 module.exports = router;
