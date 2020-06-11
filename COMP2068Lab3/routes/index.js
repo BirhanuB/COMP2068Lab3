9efecde3c1695bed4cf59b74e238f2b3bd4f61e3
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2020-06-11 17:09:23 -0400

    Created website structure and added parents info

--- COMP2068Lab3/routes/index.js
+++ COMP2068Lab3/routes/index.js
@@ -4,7 +4,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'My Family' });
+});
+
+/* GET mom's page. */
+router.get('/abebech', function (req, res) {
+    res.render('abebech', { name: 'Abebech' });
+});
+
+/* GET dad's page. */
+router.get('/bogale', function (req, res) {
+    res.render('bogale', { name: 'Bogale' });
 });
 
 module.exports = router;
